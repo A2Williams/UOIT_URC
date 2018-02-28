1ba9e73a2d0c6de9781a083044c474b4e4082031
Aaron Williams <aaronchristoph.williams@uoit.net>
2018-02-28 11:43:57 -0500

    Initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/oem/rover_sys/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/oem/rover_sys/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/oem/rover_sys/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/oem/rover_sys/src:$ROS_PACKAGE_PATH"
